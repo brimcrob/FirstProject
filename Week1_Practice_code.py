8087ce3489ae745457ae5e7dc6bd979777b7ba38
Brian McRoberts <brimcrob@gmail.com>
2022-01-15 14:25:36 +0000

    Week1 update3 further practice with If Statements and the like

--- Week1_Practice_code.py
+++ Week1_Practice_code.py
@@ -12,13 +12,23 @@ def ask_name():
 
 # ask if they want to play the game
 def play_game(name_answer):
-    game_answer = input("Do you want to play. Answer Y or N:")
-    if game_answer == "Y":
-        print("Great Choice " + name_answer + "lets play ")
+    game_answer = input("Do you want to play. Answer y or n:")
+    if game_answer == "y":
+        print("Great Choice " + name_answer + " lets play ")
     else:
         print("Never mind next time " + name_answer)
     return game_answer
 
 
+def question_one(name_answer):
+    q_1_answer = "Edinburgh"
+    question1 = input("Q1: What is the capital of Scotland")
+    if q_1_answer == "Edinburgh":
+        print("Correct. Well done " + name_answer + "Keep going")
+    else:
+        print("Wrong. Better Luck next time" + name_answer)
+
+
 name = ask_name()
 play_game(name_answer=name)
+question_one(name_answer=name)
